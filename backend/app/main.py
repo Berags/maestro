153e1cfb2c2d9734b9db40a7d12feae71be09dd3
Jacopo Beragnoli <31521001+Berags@users.noreply.github.com>
2024-04-03 22:10:38 +0200

    Added backend and docker-compose
    Added first backend files and created docker files

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,12 +4,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from typing import Optional
+
+from sqlmodel import Field, SQLModel, create_engine, Session, select
+from app.config import settings
+
+
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: Optional[int] = None
+
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
+SQLModel.metadata.create_all(engine)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/hero/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    with Session(engine) as session:
+        heroes = session.exec(select(Hero).where(Hero.id == item_id)).all()
+        return heroes
